a5aa7e72c6850902cc9727996edb3317b38b9b5d
frontendhubs <121153168+frontendhubs@users.noreply.github.com>
2023-02-13 10:43:30 +0530

    datatype

---
+++ javascript-tutorial/module-2/datatype.js
@@ -0,0 +1,87 @@
+
+// Primitive Types
+
+/*String
+Number
+Boolean
+Undefined
+Null*/
+
+// Reference Types
+
+/*Object
+
+Array
+
+function */
+
+
+//string
+
+var a = "hello world";
+
+console.log(a)
+
+console.log(typeof a)
+
+//number
+
+var b = 100;
+
+console.log(b)
+
+console.log(typeof b)
+
+//boolean
+
+var c = true;
+
+console.log(c, typeof c)
+
+//null
+
+var d = null
+
+console.log(d, typeof d)
+
+
+//undefined
+
+var e;
+
+console.log(e, typeof e)
+
+
+//Object
+
+ var laptop = {}
+
+
+
+ laptop =
+ {
+    modal : "Dell",
+    color : "white",
+    processor : "i5",
+    isAvailable : true
+ }
+ console.log(laptop, typeof laptop)
+
+
+ // array
+
+  var x = ["john", 18, true];
+
+  console.log(x, typeof x)
+
+
+  //function
+
+  function hello()
+  {
+    console.log("hello World");
+  }
+
+  hello();
+
+  console.log(typeof hello)
