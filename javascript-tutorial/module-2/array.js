9eade53fdb345755acfb104d62ef0e6d5949d58c
faizalkhan <faizalkhan.s@gmail.com>
2024-01-27 12:49:53 +0530

    updated the script

---
+++ javascript-tutorial/module-2/array.js
@@ -0,0 +1,28 @@
+var arr = [];
+
+console.log(arr);
+
+arr = ["hello", 50, true];
+
+console.log(arr);
+
+//Accessing the  array value
+
+// var a = arr[1]; 
+
+console.log(arr[1])
+
+//Adding  the  new array value
+arr[3] = "Apple"
+
+console.log(arr)
+
+// replacing the array value
+
+arr[0] ="Welcome to frontend Hub"
+
+console.log(arr)
+
+//finding the array length
+
+console.log(arr.length)
